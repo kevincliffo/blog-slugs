c0266519583c5e510df4edf2dc83093b4d34cd7b
kevincliffo <kevincliffo@gmail.com>
2024-01-16 21:35:42 +0300

    New

--- articles/models.py
+++ articles/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.urls import reverse
+from django.template.defaultfilters import slugify 
 
 class Article(models.Model):
     title = models.CharField(max_length=255)
@@ -11,3 +12,8 @@ class Article(models.Model):
 
     def get_absolute_url(self):
         return reverse("articles:article_detail", kwargs={"slug": self.slug})
+    
+    def save(self, *args, **kwargs):  # new
+        if not self.slug:
+            self.slug = slugify(self.title)
+        return super().save(*args, **kwargs)
