f7536f195c114b610a0ba58ce1ba0e6216f6907f
kevincliffo <kevincliffo@gmail.com>
2024-01-16 21:35:53 +0300

    New

--- articles/models.py
+++ articles/models.py
@@ -13,7 +13,7 @@ class Article(models.Model):
     def get_absolute_url(self):
         return reverse("articles:article_detail", kwargs={"slug": self.slug})
     
-    def save(self, *args, **kwargs):  # new
-        if not self.slug:
-            self.slug = slugify(self.title)
-        return super().save(*args, **kwargs)
+    # def save(self, *args, **kwargs):  # new
+    #     if not self.slug:
+    #         self.slug = slugify(self.title)
+    #     return super().save(*args, **kwargs)
